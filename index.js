ac258e64882c8fc155b0b05a974fc7a947fcf28a
AnandBharati <anandbharati0402@gmail.com>
2023-01-11 07:08:40 +0530

    first commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.listen('5000')
+app.listen(process.env.PORT || '5000')
 
 app.get('/', (req, res) => {
     res.status(200).json('working')
